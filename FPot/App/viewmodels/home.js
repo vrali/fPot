2b58bbe5973f19b18f35080403cdba5dbad8cfee
vrali <vinay.rali@gmail.com>
2014-12-15 16:46:35 -0500

    home page changes

--- FPot/App/viewmodels/home.js
+++ FPot/App/viewmodels/home.js
@@ -1,18 +1,36 @@
 ﻿define(['durandal/app'], function (app) {
-    var that = this
+    var that = this;
     that.search = function() { app.showMessage('Search not yet implemented...'); },
-        that.activeDate = ko.observable(new Date()),
-        that.days = ko.observableArray([]),
+        that.activeDate = ko.observable(),
+        that.days = ko.observableArray(),
         that.activate = function() {
             var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            for (var i = 0; i < 14; i++) {
+            for (var i = 1; i < 14; i++) {
                 var date = new Date();
                 date.setDate(date.getDate() + i);
-                this.days.push({ date: date, day: weekdays[date.getDay()] });
+                that.days.push({
+                    date: date, formattedDate: function () {
+                        //if (i == 1)
+                        //    return "Tommorow " + this.date.toDateString().slice(4);
+                        //else
+                            return date.toDateString();
+                    }()});
             };
+            that.activeDate(that.days()[0].date);
+            that.selectedDate = ko.computed(
+    {
+        read: function () {
+            return that.activeDate().toDateString();
+        },
+        write: function (selectedDay) {
+            that.activeDate(selectedDay.date);
+        }
+    });
+
         };
+    
     that.select = function (selectedDay) {
-        that.activeDate(selectedDay.date);
+        that.selectedDate(selectedDay);
     };
 
     return that;
